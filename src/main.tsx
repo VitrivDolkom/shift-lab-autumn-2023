210777a61a8f9c9f1b6b33b7e1d8a57405fb094a
Volkov Dmitriy <vdy0555@gmail.com>
2023-09-19 13:08:32 +0700

    linter and formatter configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./components/App/App"
-import "./index.css"
+import './index.css'
+import App from './components/App/App'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
