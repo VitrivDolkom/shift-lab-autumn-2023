d4b86a7078775e87def3628ed68e9f6aba5b8f47
Volkov Dmitriy <vdy0555@gmail.com>
2023-09-20 14:38:25 +0700

    renaming, fonts, api
    renamed App folder
    load static fonts
    separate api stuff

--- src/api/table.ts
+++ src/api/table.ts
@@ -1,4 +1,4 @@
-const distrosTable: DistrosTable = {
+export const distrosTable: DistrosTable = {
   rows: [
     ['Name', 'Users feedback', 'User experience', 'Company name', 'Origin'],
     [
@@ -30,34 +30,4 @@ const distrosTable: DistrosTable = {
       'Worldwide'
     ]
   ]
-}
-
-export const fetchDistros = (): Promise<DistrosTable> =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(distrosTable)
-    }, 1000)
-  })
-
-export const postSearch = (dto: TableSearchDto): Promise<DistrosTableRow> => {
-  const rowIndex = distrosTable.rows[0].indexOf(dto.header)
-  const arr: DistrosTableRow = []
-  let errorMessage = ''
-
-  if (rowIndex === -1) errorMessage = 'Header not valid'
-
-  for (let i = 1; i < distrosTable.rows.length; i++) {
-    const row = distrosTable.rows[i]
-
-    if (row[rowIndex].toLowerCase().indexOf(dto.search.toLowerCase()) > -1) {
-      arr.push(row[rowIndex])
-    }
-  }
-
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (!!errorMessage) reject(errorMessage)
-      resolve(arr)
-    }, 1000)
-  })
 }
